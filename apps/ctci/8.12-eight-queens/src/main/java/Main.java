e0fcb7dfdb6922e1bf7cb6f8d1eeaddba4aac3bf
Paul Michael Reilly <pmr@pajato.com>
2017-09-25 00:43:21 -0400

    Add the CTCI solution to the 8-queens problem.

--- apps/ctci/8.12-eight-queens/src/main/java/Main.java
+++ apps/ctci/8.12-eight-queens/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Problem 8.12 Eight Queens
  *
@@ -13,49 +16,47 @@ public class Main {
     /** Return a list of all solutions to the eight queens problem, generalized to an nxn board. */
     static List<String> getSolutions(final int n) {
         // Handle the special case, n < 4.
-        if (n < 4)
+        if (n <= 0 || n == 2 || n == 3)
             return new ArrayList<String>();
 
-        // Handle the normal cases, n >= 4 using recursion to detect all possible solutions.
+        // Handle the normal case, n > 4
         List<String> result = new ArrayList<>();
-        checkSolutions(new int[n][n], 0, 0, result);
+        checkSolutions(0, new int[n], result);
         return result;
     }
 
-    private static void checkSolutions(final int[][] board, final int r, final int c,
-                                       final List<String> result) {
-        // Handle the base case of a successful board.
-        final int n = board.length;
-        if (r > n - 1) {
-            // This board is a solution.  Add it to the result.
-            result.add("got one");
-            return;
-        }
-
-        // Handle the base case of an unsuccessful board.
-        if (c > n - 1)
-            return;
-
-        // Deal with the normal case, place a queen at (r,c) and test it.
-        board[r][c] = 1;
-        if (threatened(board, r, c)) {
-            // This position will not work.  Try the next column in this row.
-            board[r][c] = 0;
-            checkSolutions(board, r, c + 1);
-        } else {
-            // This position does work, try the next row.
-            checkSolutions(board, r + 1, 0)
-        }
-
-        // Backtrack and test for completion.
-        // tbc
-        if (c > n - 1)
-            // Backtrack the row.
+    private static void checkSolutions(final int row, final int[] columns, final List<String> result) {
+        final int n = columns.length;
+        if (row == n)
+            result.add("Got one!");
+        else
+            for (int col = 0; col < n; col++)
+                if (isValid(columns, row, col)) {
+                    columns[row] = col;
+                    checkSolutions(row + 1, columns, result);
+                }
     }
 
-    /** Return TRUE iff the piece at the given cell is threatened by another queen. */
-    private static boolean threatened(final int[][] board, int r, int c) {
-        // tbc.
-        return false;
+    /** Return TRUE iff the piece at the given cell is a valid position for a queen to be places. */
+    private static boolean isValid(final int[] columns, int row, int col) {
+        // The queen at position (r, c) is threatened if there is another queen on the board with
+        // the same row or column or is on the same diagonal.
+        // Check for another queen on the same row.
+        int n = columns.length;
+        for (int r = 0; r < row; r++) {
+            // Determine if (r, c) invalidates (row, col)
+            int c = columns[r];
+            if (col == c)
+                return false;
+
+            // Check for another queen on the same diagonal.
+            int colDistance = Math.abs(c - col);
+            int rowDistance = row - r;
+            if (colDistance == rowDistance)
+                return false;
+        }
+
+        // A queen can be placed at (row, col)
+        return true;
     }
 }
--- apps/ctci/8.12-eight-queens/src/test/java/MainTest.java
+++ apps/ctci/8.12-eight-queens/src/test/java/MainTest.java
@@ -1,7 +1,10 @@
 import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.List;
 
 /**
- * Test generating the permutations of a string with no duplicate characters.
+ * Test the getSolutions() method.
  *
  * @author Paul Michael Reilly
  */
@@ -14,34 +17,55 @@ public class MainTest {
 
     /** Main tests for the next() method. */
     @Test public void test1() {
-        runTest(1, null);
+        runTest(1, 0, 0);
     }
 
     @Test public void test2() {
-        runTest(2, "");
+        runTest(2, 1, 1);
     }
 
     @Test public void test3() {
-        runTest(3, "ab");
+        runTest(3, 2, 0);
     }
 
     @Test public void test4() {
-        runTest(4, "abc");
+        runTest(4, 3, 0);
     }
 
     @Test public void test5() {
-        runTest(5, "abcd");
+        runTest(5, 4, 2);
     }
 
     @Test public void test6() {
-        runTest(6, "abcde");
+        runTest(6, 5, 10);
+    }
+
+    @Test public void test7() {
+        runTest(7, 6, 4);
+    }
+
+    @Test public void test8() {
+        runTest(8, 7, 40);
+    }
+
+    @Test public void test9() {
+        runTest(9, 8, 92);
+    }
+
+    @Test public void test10() {
+        runTest(10, 9, 352);
+    }
+
+    @Test public void test11() {
+        runTest(11, 10, 724);
     }
 
     // Private instance methods.
 
     /** Run a test with a given test number and input string. */
-    private void runTest(final int testNum, final String input) {
+    private void runTest(final int testNum, final int n, final int expected) {
         System.out.println("Running test number " + testNum);
-        Main.printPermutations(input);
+        List<String> actual = Main.getSolutions(n);
+        Assert.assertEquals("The number of solutions is wrong!", expected, actual.size());
     }
 }
